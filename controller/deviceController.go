2bb2df0ee414df2a1bb963572b431295d89eec5b
breaknewstrader <crazyhacker680@outlook.com>
2025-02-10 15:06:24 -0800

    fix: config device

--- controller/deviceController.go
+++ controller/deviceController.go
@@ -20,121 +20,26 @@ type RequestConnectParams struct {
 	AccessToken  string   `json:"accessToken"`
 	RefreshToken string   `json:"refreshToken"`
 }
-
-type TelemetryRequest struct {
-	Config struct {
-		Port   int `json:"port"`
-		Exp    int `json:"exp"`
-		Fields struct {
-			Location struct {
-				IntervalSeconds int `json:"interval_seconds"`
-			} `json:"Location"`
-			ChargeState struct {
-				IntervalSeconds int `json:"interval_seconds"`
-			} `json:"ChargeState"`
-		} `json:"fields"`
-		CA       string `json:"ca"`
-		Hostname string `json:"hostname"`
-	} `json:"config"`
-	VINs []string `json:"vins"`
+type FieldConfig struct {
+	ResendIntervalSeconds int `json:"resend_interval_seconds"`
+	MinimumDelta          int `json:"minimum_delta"`
+	IntervalSeconds       int `json:"interval_seconds"`
 }
 
-// func ConnectDevice_backup(c *gin.Context) {
-// 	var requestParams RequestConnectParams
-// 	if err := c.ShouldBindJSON(&requestParams); err != nil {
-// 		fmt.Println(err.Error())
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
+type Config struct {
+	PreferTyped bool                   `json:"prefer_typed"`
+	Port        int                    `json:"port"`
+	Exp         int64                  `json:"exp"`
+	AlertTypes  []string               `json:"alert_types"`
+	Fields      map[string]FieldConfig `json:"fields"`
+	CA          string                 `json:"ca"`
+	Hostname    string                 `json:"hostname"`
+}
 
-// 	base := config.GetTeslaCredential().ProxyUri
-// 	path := "/api/1/vehicles/fleet_telemetry_config"
-// 	url := fmt.Sprintf("%s%s", base, path)
-
-// 	telemetryData := TelemetryRequest{}
-// 	telemetryData.Config.Port = 8443
-// 	telemetryData.Config.Exp = 1750000000
-// 	telemetryData.Config.Fields.Location.IntervalSeconds = 500
-// 	telemetryData.Config.Fields.ChargeState.IntervalSeconds = 500
-// 	telemetryData.Config.CA = config.GetTeslaCredential().Certificate
-
-// 	telemetryData.Config.Hostname = config.GetTeslaCredential().ServerDomain
-// 	telemetryData.VINs = requestParams.Vins
-
-// 	payload, err := json.Marshal(telemetryData)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{
-// 			"msg":   "Failed to marshal JSON data:",
-// 			"error": err,
-// 		})
-// 		return
-// 	}
-
-// 	cert, err := tls.X509KeyPair(
-// 		[]byte(config.GetTeslaCredential().ClientCert), // Your client certificate
-// 		[]byte(config.GetTeslaCredential().ClientKey),  // Your client private key
-// 	)
-
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"msg": "failed to load client certificate", "error": err.Error()})
-// 		return
-// 	}
-
-// 	caCert := []byte(config.GetTeslaCredential().Certificate) // CA certificate
-// 	caCertPool := x509.NewCertPool()
-// 	if !caCertPool.AppendCertsFromPEM(caCert) {
-// 		log.Fatalf("Failed to append CA certificate")
-// 	}
-
-// 	// Create a custom TLS configuration with client certificate
-// 	tlsConfig := &tls.Config{
-// 		Certificates: []tls.Certificate{cert}, // Include client certificate
-// 		RootCAs:      caCertPool,              // Include CA certificate
-// 	}
-
-// 	client := &http.Client{
-// 		Transport: &http.Transport{
-// 			TLSClientConfig: tlsConfig,
-// 		},
-// 		Timeout: 30 * time.Second,
-// 	}
-
-// 	// Create HTTP request
-// 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{
-// 			"msg":   "failed to create HTTP request",
-// 			"error": err,
-// 		})
-// 		return
-// 	}
-
-// 	// Set headers
-// 	req.Header.Set("Content-Type", "application/json")
-// 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", requestParams.AccessToken))
-
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{
-// 			"msg":      "failed to send HTTP request:",
-// 			"error":    err,
-// 			"jsonData": telemetryData,
-// 			"payload":  payload,
-// 		})
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	body, _ := ioutil.ReadAll(resp.Body)
-
-// 	var jsonData map[string]interface{}
-// 	json.Unmarshal([]byte(string(body)), &jsonData)
-
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"msg":  "done!",
-// 		"data": jsonData,
-// 	})
-// }
+type TelemetryRequest struct {
+	Config Config   `json:"config"`
+	Vins   []string `json:"vins"`
+}
 
 func ConnectDevice(c *gin.Context) {
 	var requestParams RequestConnectParams
@@ -148,15 +53,27 @@ func ConnectDevice(c *gin.Context) {
 	path := "/api/1/vehicles/fleet_telemetry_config"
 	url := fmt.Sprintf("%s%s", base, path)
 
-	telemetryData := TelemetryRequest{}
-	telemetryData.Config.Port = 8443
-	telemetryData.Config.Exp = 1750000000
-	telemetryData.Config.Fields.Location.IntervalSeconds = 500
-	telemetryData.Config.Fields.ChargeState.IntervalSeconds = 500
-	telemetryData.Config.CA = config.GetTeslaCredential().Certificate
+	fieldToStream := "speed" // Replace with any dynamic field name
 
-	telemetryData.Config.Hostname = config.GetTeslaCredential().ServerDomain
-	telemetryData.VINs = requestParams.Vins
+	// Create your telemetry request struct with the desired values.
+	telemetryData := TelemetryRequest{
+		Config: Config{
+			PreferTyped: true,
+			Port:        4443,
+			Exp:         1704067200,
+			AlertTypes:  []string{"service"},
+			Fields: map[string]FieldConfig{
+				fieldToStream: {
+					ResendIntervalSeconds: 3600,
+					MinimumDelta:          1,
+					IntervalSeconds:       1800,
+				},
+			},
+			CA:       config.GetTeslaCredential().Certificate,
+			Hostname: config.GetTeslaCredential().ServerDomain,
+		},
+		Vins: requestParams.Vins,
+	}
 
 	payload, err := json.Marshal(telemetryData)
 	if err != nil {
