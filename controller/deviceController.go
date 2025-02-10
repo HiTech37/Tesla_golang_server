c41db5217d36ff1fccc145327613c3f6b9d44df3
breaknewstrader <crazyhacker680@outlook.com>
2025-02-10 13:50:45 -0800

    fix: config device

--- controller/deviceController.go
+++ controller/deviceController.go
@@ -39,6 +39,103 @@ type TelemetryRequest struct {
 	VINs []string `json:"vins"`
 }
 
+// func ConnectDevice_backup(c *gin.Context) {
+// 	var requestParams RequestConnectParams
+// 	if err := c.ShouldBindJSON(&requestParams); err != nil {
+// 		fmt.Println(err.Error())
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	base := config.GetTeslaCredential().ProxyUri
+// 	path := "/api/1/vehicles/fleet_telemetry_config"
+// 	url := fmt.Sprintf("%s%s", base, path)
+
+// 	telemetryData := TelemetryRequest{}
+// 	telemetryData.Config.Port = 8443
+// 	telemetryData.Config.Exp = 1750000000
+// 	telemetryData.Config.Fields.Location.IntervalSeconds = 500
+// 	telemetryData.Config.Fields.ChargeState.IntervalSeconds = 500
+// 	telemetryData.Config.CA = config.GetTeslaCredential().Certificate
+
+// 	telemetryData.Config.Hostname = config.GetTeslaCredential().ServerDomain
+// 	telemetryData.VINs = requestParams.Vins
+
+// 	payload, err := json.Marshal(telemetryData)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"msg":   "Failed to marshal JSON data:",
+// 			"error": err,
+// 		})
+// 		return
+// 	}
+
+// 	cert, err := tls.X509KeyPair(
+// 		[]byte(config.GetTeslaCredential().ClientCert), // Your client certificate
+// 		[]byte(config.GetTeslaCredential().ClientKey),  // Your client private key
+// 	)
+
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"msg": "failed to load client certificate", "error": err.Error()})
+// 		return
+// 	}
+
+// 	caCert := []byte(config.GetTeslaCredential().Certificate) // CA certificate
+// 	caCertPool := x509.NewCertPool()
+// 	if !caCertPool.AppendCertsFromPEM(caCert) {
+// 		log.Fatalf("Failed to append CA certificate")
+// 	}
+
+// 	// Create a custom TLS configuration with client certificate
+// 	tlsConfig := &tls.Config{
+// 		Certificates: []tls.Certificate{cert}, // Include client certificate
+// 		RootCAs:      caCertPool,              // Include CA certificate
+// 	}
+
+// 	client := &http.Client{
+// 		Transport: &http.Transport{
+// 			TLSClientConfig: tlsConfig,
+// 		},
+// 		Timeout: 30 * time.Second,
+// 	}
+
+// 	// Create HTTP request
+// 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"msg":   "failed to create HTTP request",
+// 			"error": err,
+// 		})
+// 		return
+// 	}
+
+// 	// Set headers
+// 	req.Header.Set("Content-Type", "application/json")
+// 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", requestParams.AccessToken))
+
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"msg":      "failed to send HTTP request:",
+// 			"error":    err,
+// 			"jsonData": telemetryData,
+// 			"payload":  payload,
+// 		})
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+
+// 	body, _ := ioutil.ReadAll(resp.Body)
+
+// 	var jsonData map[string]interface{}
+// 	json.Unmarshal([]byte(string(body)), &jsonData)
+
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"msg":  "done!",
+// 		"data": jsonData,
+// 	})
+// }
+
 func ConnectDevice(c *gin.Context) {
 	var requestParams RequestConnectParams
 	if err := c.ShouldBindJSON(&requestParams); err != nil {
@@ -70,56 +167,24 @@ func ConnectDevice(c *gin.Context) {
 		return
 	}
 
-	cert, err := tls.X509KeyPair(
-		[]byte(config.GetTeslaCredential().ClientCert), // Your client certificate
-		[]byte(config.GetTeslaCredential().ClientKey),  // Your client private key
-	)
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"msg": "failed to load client certificate", "error": err.Error()})
-		return
-	}
-
-	caCert := []byte(config.GetTeslaCredential().Certificate) // CA certificate
-	caCertPool := x509.NewCertPool()
-	if !caCertPool.AppendCertsFromPEM(caCert) {
-		log.Fatalf("Failed to append CA certificate")
-	}
-
-	// Create a custom TLS configuration with client certificate
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{cert}, // Include client certificate
-		RootCAs:      caCertPool,              // Include CA certificate
-	}
-
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: tlsConfig,
-		},
-		Timeout: 30 * time.Second,
-	}
-
-	// Create HTTP request
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"msg":   "failed to create HTTP request",
+			"msg":   "Error creating request:",
 			"error": err,
 		})
 		return
 	}
 
-	// Set headers
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", requestParams.AccessToken))
 
+	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"msg":      "failed to send HTTP request:",
-			"error":    err,
-			"jsonData": telemetryData,
-			"payload":  payload,
+			"msg":   "Error making request:",
+			"error": err,
 		})
 		return
 	}
