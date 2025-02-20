501ace74d30959c1437c4cda0ea3be2475270b49
breaknewstrader <crazyhacker680@outlook.com>
2025-02-20 04:27:22 -0800

    fix: port of fleet server

--- controller/deviceController.go
+++ controller/deviceController.go
@@ -58,7 +58,7 @@ func ConnectDevice(c *gin.Context) {
 	telemetryData := TelemetryRequest{
 		Config: Config{
 			PreferTyped: true,
-			Port:        5443,
+			Port:        4443,
 			Exp:         1770670000,
 			AlertTypes:  []string{"service"},
 			Fields: map[string]FieldConfig{
