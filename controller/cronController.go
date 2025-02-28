5c3477e029bef5f483c9b27e4320c63b19650e25
breaknewstrader <crazyhacker680@outlook.com>
2025-02-28 09:03:11 -0800

    fix: device signup

--- controller/cronController.go
+++ controller/cronController.go
@@ -8,7 +8,7 @@ import (
 
 func CronJobs() {
 	checkDeviceCreditTicker := time.NewTicker(1 * time.Minute)
-	hourTicker := time.NewTicker(1 * time.Minute)
+	hourTicker := time.NewTicker(10 * time.Second)
 	go func() {
 		for {
 			select {
