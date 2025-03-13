df5cf1948d2ce3bcb95efbd7962cca020d166151
breaknewstrader <crazyhacker680@outlook.com>
2025-03-13 14:19:48 -0700

    fix: cert.pem

--- config/environment.go
+++ config/environment.go
@@ -50,7 +50,7 @@ M4FqRaFSnjY1EadGi2UPCEqk8fARdUwA0EmN
 	teslaCredential.Certificate = certificate
 	teslaCredential.ServerDomain = "fleetapi.moovetrax.com"
 	teslaCredential.Port = 8443
-	teslaCredential.ProxyUri = "https://fleetapi.moovetrax.com:4443"
+	teslaCredential.ProxyUri = "https://localhost:4443"
 	// teslaCredential.ProxyUri = "https://localhost:4443"
 
 	teslaCredential.TestServerUri = "https://test.moovetrax.com:8088"
